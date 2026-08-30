3dcaeb7a1ed65ca94efea381c23c7607671de29c
Marcus Camargo <75137005+Marcus-W-Camargo@users.noreply.github.com>
2026-08-30 12:49:04 -0300

    load refined project preview styles

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './contact-overrides.css'
 import './section-kicker-overrides.css'
 import './header-overrides.css'
 import './github-button-overrides.css'
+import './project-preview-overrides.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
