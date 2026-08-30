12768b1bc53d85ac30f47245091b1cbda752989b
Marcus Camargo <75137005+Marcus-W-Camargo@users.noreply.github.com>
2026-08-30 12:09:37 -0300

    style: load about section overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './identity-overrides.css'
 import './hero-stats-overrides.css'
+import './about-overrides.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
